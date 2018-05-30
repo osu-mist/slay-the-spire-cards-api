d6c96a877c48b98ad734e91c51293166a5dd9fa6
ianbrown9475 <ian.brown9475@gmail.com>
2018-05-30 15:54:33 -0700

    Add POST functionality

--- src/main/groovy/edu/oregonstate/mist/cardsapi/resources/CardsResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/cardsapi/resources/CardsResource.groovy
@@ -8,10 +8,13 @@ import edu.oregonstate.mist.cardsapi.db.CardDAO
 import edu.oregonstate.mist.api.Resource
 import edu.oregonstate.mist.api.jsonapi.ResourceObject
 import edu.oregonstate.mist.api.jsonapi.ResultObject
+import io.dropwizard.validation.Validated
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import javax.ws.rs.Consumes
 import javax.ws.rs.DELETE
+import javax.ws.rs.POST
 import javax.annotation.security.PermitAll
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -96,8 +99,6 @@ class CardsResource extends Resource {
                       @QueryParam("number") Optional<Integer> number,
                       @QueryParam("isRandom") Optional<Boolean> isRandom) {
 
-        Response response
-
         List<String> validTypes = ["skill", "attack", "power", "status", "curse"]
         List<String> validColors = ["red", "green", "blue", "colorless"]
         List<String> validRarities = ["basic", "common", "uncommon", "rare"]
@@ -107,8 +108,7 @@ class CardsResource extends Resource {
         } else {
             for(int i = 0; i < types.size(); i++) {
                 if(!validTypes.contains(types[i])) {
-                    response = badRequest("Invalid types").build()
-                    return response
+                    return badRequest("Invalid types").build()
                 }
             }
         }
@@ -118,8 +118,7 @@ class CardsResource extends Resource {
         } else {
             for(int i = 0; i < colors.size(); i++) {
                 if(!validColors.contains(colors[i])) {
-                    response = badRequest("Invalid colors").build()
-                    return response
+                    return badRequest("Invalid colors").build()
                 }
             }
         }
@@ -129,21 +128,18 @@ class CardsResource extends Resource {
         } else {
             for(int i = 0; i < rarities.size(); i++) {
                 if(!validRarities.contains(rarities[i])) {
-                    response = badRequest("Invalid rarities").build()
-                    return response
+                    return badRequest("Invalid rarities").build()
                 }
             }
         }
 
         if(!(name.or("")).matches('[a-zA-Z0-9 ."+-]*')) {
-            response = badRequest("Invalid name").build()
-            return response
+            return badRequest("Invalid name").build()
         }
 
         for(int i = 0; i < keywords.size(); i++) {
             if(!keywords[i].matches('[a-zA-Z0-9 ."+-]*')) {
-                response = badRequest("Invalid use of keywords").build()
-                return response
+                return badRequest("Invalid use of keywords").build()
             }
         }
 
@@ -152,12 +148,36 @@ class CardsResource extends Resource {
                 keywords, number.or(10), isRandom.or(true))
 
         ResultObject cardResult = cardsResult(cards)
-        response = ok(cardResult).build()
-        response
+        ok(cardResult).build()
+    }
+
+    @POST
+    @Path('')
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response postCard (@Validated Card newCard) {
+
+        if(!cardValidator(newCard)) {
+            return badRequest("Invalid card object").build()
+        }
+
+        Integer id = cardDAO.getNextId()
+        cardDAO.postCard(
+                id,
+                newCard.type,
+                newCard.name,
+                newCard.color,
+                newCard.rarity,
+                newCard.energy,
+                newCard.description)
+
+        Card card = cardDAO.getCardById(id)
+        ResultObject cardResult = cardsResult(card)
+        created(cardResult).build()
     }
 
     // Converts list of strings to comma-separated list in SQL
-    String listToSql (List<String> list) {
+    static String listToSql (List<String> list) {
         String str = "("
         for(int i = 0; i < list.size() - 1; i++) {
             str += "\'" + list[i] + "\',"
@@ -167,7 +187,7 @@ class CardsResource extends Resource {
     }
 
     // Builds list of LIKE statements for each string in keywords
-    String keywordsToSql (List<String> keywords) {
+    static String keywordsToSql (List<String> keywords) {
         String str = ""
         for(int i = 0; i < keywords.size(); i++) {
             str += "AND LOWER(DESCRIPTION) LIKE LOWER('%'||"
@@ -238,5 +258,23 @@ class CardsResource extends Resource {
         List<Card> cards = q.list()
         h.close()
         cards
+    }
+
+    static boolean cardValidator(Card card) {
+        List<String> validTypes = ["skill", "attack", "power", "status", "curse"]
+        List<String> validColors = ["red", "green", "blue", "colorless"]
+        List<String> validRarities = ["basic", "common", "uncommon", "rare"]
+
+        if(!(validTypes.contains(card.type)
+                && validColors.contains(card.color)
+                && validRarities.contains(card.rarity)
+                && card.name.matches('[a-zA-Z0-9 ."+-]*')
+                && card.description.matches('[a-zA-Z0-9 ."+-]*')
+                && card.energy >= 0
+                && card.energy <= 999)) {
+            false
+        } else {
+            true
+        }
     }
 }
